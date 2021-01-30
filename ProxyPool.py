fc35aed4c602aeff168f3dc303e519d23c327a7b
liuyun <liuyunplus@gmail.com>
2021-01-30 11:47:52 +0800

    fix

--- ProxyPool.py
+++ ProxyPool.py
@@ -1,10 +1,24 @@
 import os
+import requests
 import Settings
+import random
 
 
 def get_proxy():
-    files = list(filter(lambda f: "|" not in f, os.listdir(Settings.POOL_DIR)))
-    if not files:
-        return None
-    files.sort(key=lambda f: os.path.getmtime(os.path.join(Settings.POOL_DIR, f)))
-    return files[0]
+    while True:
+        files = list(filter(lambda f: "|" not in f, os.listdir(Settings.POOL_DIR)))
+        if not files:
+            return None
+        ip_port = random.choice(files)
+        # files.sort(key=lambda f: os.path.getmtime(os.path.join(Settings.POOL_DIR, f)))
+        if check(ip_port):
+            break
+    return ip_port
+
+def check(ip_port):
+    proxies = {"http": "http://" + ip_port, "https": "https://" + ip_port}
+    try:
+        requests.get("http://www.baidu.com", proxies=proxies, timeout=1)
+        return True
+    except Exception:
+        return False
